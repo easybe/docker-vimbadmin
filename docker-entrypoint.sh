210904e711c5ea90a18ced69a99b5adaa915a864
Ezra Buehler <spam@easyb.ch>
2016-03-27 05:32:50 +0000

    Clean up docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
+
+MYSQL_ROOT="mysql -u root -p${DB_ENV_MYSQL_ROOT_PASSWORD} -h db"
+MYSQL="mysql -u vimbadmin -p${DB_ENV_MYSQL_PASSWORD} -h db"
 
 sed -i "s/PRIMARY_HOSTNAME/${HOSTNAME}/g"  ${INSTALL_PATH}/public/mail/config-v1.1.xml
 sed -i "s/PRIMARY_HOSTNAME/${HOSTNAME}/g"  ${INSTALL_PATH}/public/mail.mobileconfig.php
@@ -19,20 +22,19 @@ sed -i "s/ADMIN_EMAIL/${ADMIN_EMAIL}/g" ${INSTALL_PATH}/application/configs/appl
 
 cat /salts >> ${INSTALL_PATH}/application/configs/application.ini
 
-for ((i=0;i<10;i++))
-do
-    DB_CONNECTABLE=$(mysql -uvimbadmin -p${DB_ENV_MYSQL_PASSWORD} -hdb -P3306 -e 'status' >/dev/null 2>&1; echo "$?")
-    if [[ DB_CONNECTABLE -eq 0 ]]; then
-      if [ $(mysql -N -s -uvimbadmin -p${DB_ENV_MYSQL_PASSWORD} -hdb -e \
-        "select count(*) from information_schema.tables where \
-          table_schema='vimbadmin' and table_name='domain';") -ne 1 ]; then
-        echo "Creating DB and Superuser"
-        HASH_PASS=`php -r "echo password_hash('${ADMIN_PASSWORD}', PASSWORD_DEFAULT);"`
-        ./bin/doctrine2-cli.php orm:schema-tool:create
-        mysql -u vimbadmin -p${DB_ENV_MYSQL_PASSWORD} -h db vimbadmin -e \
-          "INSERT INTO admin (username, password, super, active, created, modified) VALUES ('${ADMIN_EMAIL}', '$HASH_PASS', 1, 1, NOW(), NOW())" && \
-        echo "Vimbadmin setup completed successfully"
-      fi
+for ((i=0;i<10;i++)); do
+    if $MYSQL_ROOT -e 'status' >/dev/null 2>&1; then
+        if [ $($MYSQL_ROOT -N -s -e "SELECT COUNT(*) FROM information_schema.tables WHERE \
+          table_schema='vimbadmin' AND table_name='domain';") -ne 1 ]; then
+            echo "Creating DB and Superuser"
+            $MYSQL_ROOT -e "CREATE DATABASE IF NOT EXISTS vimbadmin;"
+            $MYSQL_ROOT -e "GRANT ALL ON vimbadmin.* TO 'vimbadmin'@'%' IDENTIFIED BY '"${DB_ENV_MYSQL_PASSWORD}"';"
+            ./bin/doctrine2-cli.php orm:schema-tool:create
+            HASH_PASS=`php -r "echo password_hash('${ADMIN_PASSWORD}', PASSWORD_DEFAULT);"`
+            $MYSQL vimbadmin -e "INSERT INTO admin (username, password, super, active, created, modified) VALUES ('${ADMIN_EMAIL}', '$HASH_PASS', 1, 1, NOW(), NOW())" && \
+                echo "Vimbadmin setup completed successfully"
+        fi
+        exit 0
     fi
     sleep 5
 done
